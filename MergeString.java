6c97dc9025edddcfe4b806de8134062a4c1a2dbe
Eray-Erdogan <eray.erdogan@ozu.edu.tr>
2021-08-31 16:02:52 +0300

    Inital commit New Practices

---
+++ MergeString.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class MergeString {
+    static String a1="";
+    static String b1="";
+
+
+    public static void main(String[] args) {
+
+        // Our function will take two parameter
+        // We will merge two string in one string in char order
+
+
+
+    }
+}
